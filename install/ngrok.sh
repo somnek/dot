2456e6ae1574768c72e20cc1b340f57c671a98ea
Som Nek <38861036+somnek@users.noreply.github.com>
2022-06-30 10:59:28 +0800

    ngrok

---
+++ install/ngrok.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && \
+sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin && \
+echo "Done"
---
+++ others/cloudflare-certs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+# src: https://developers.cloudflare.com/cloudflare-one/connections/connect-devices/warp/install-cloudflare-cert/#verify-the-certificate-fingerprint
