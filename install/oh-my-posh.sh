96b7e17acb7a2c1cd657a1043f65057990a48f52
Som Nek <38861036+somnek@users.noreply.github.com>
2022-07-01 14:00:56 +0800

    oh my posh

---
+++ install/oh-my-posh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+sudo apt-get install zip && \
+sudo apt-get install unzip && \
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+oh-my-posh font install
+
+echo "If installation successfull, add 'eval \"$(oh-my-posh init bash)\"' to .bashrc or .zshrc"
+
+
+
+
+
+# src [installation](https://ohmyposh.dev/docs/installation/linux)
