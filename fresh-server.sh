e7b89e054bc5c86b91ca31f571b53abe2ca70774
Som Nek <38861036+somnek@users.noreply.github.com>
2022-08-16 16:31:17 +0800

    uses sudo

--- fresh-server.sh
+++ fresh-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y upgrade
-apt-get install vim && \
-apt-get install git && \
-apt-get install nvim && \
+sudo apt-get install vim && \
+sudo apt-get install git && \
+sudo apt-get install nvim && \
 touch ~/.bash_aliases && \
 echo "alias vi='nvim'" >> ~/.bash_aliases && \
 echo "alias lst='ls -tl'" >> ~/.bash_aliases
