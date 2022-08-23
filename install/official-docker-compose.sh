d4924204ba7c73e29c079c0163d7a0b8b22a31db
somnek <mumscarpetiswet@gmail.com>
2022-08-23 13:19:20 +0800

    git fast

--- install/official-docker-compose.sh
+++ install/official-docker-compose.sh
@@ -5,7 +5,7 @@ sudo apt-get install docker-compose-plugin
 
 VERSION_STRING=$(apt-cache madison docker-compose-plugin | awk 'NR==1{print $3}')
 sudo apt-get install docker-compose-plugin="$VERSION_STRING" && \
-docker-compose version && \
+docker compose version && \
 echo -e "\e[32mDone!\e[0m"
 
 
