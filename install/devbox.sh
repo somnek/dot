0a14e7fc3ab5f3f9d978c10660449cdcc755c845
somnek <mumscarpetiswet@gmail.com>
2022-12-29 23:50:12 +0800

    make and devbox

--- install/devbox.sh
+++ install/devbox.sh
@@ -2,4 +2,4 @@
 sh <(curl -L https://nixos.org/nix/install) --daemon
 curl -fsSL https://get.jetpack.io/devbox | bash
 
-echo "this doesn't work, you prolly don't have docker installed..."
+echo "if this doesn't work, you prolly don't have docker installed..."
