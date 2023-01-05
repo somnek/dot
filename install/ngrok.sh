32b65f4d30d998a84a5736699d606ddfd34d11ce
somnek <mumscarpetiswet@gmail.com>
2023-01-06 01:25:55 +0800

    ngrok rob's version

--- install/ngrok.sh
+++ install/ngrok.sh
@@ -2,3 +2,13 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && \
 sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin && \
 echo "Done"
+
+
+## rob's version: ##
+
+#!/bin/bash
+#set -x
+
+#curl -SsLO "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
+#mkdir -p ~/.local/bin
+#tar zxvf ngrok-v3-stable-linux-amd64.tgz -C ~/.local/bin
