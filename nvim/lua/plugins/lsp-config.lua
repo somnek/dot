14593986e34adf4e0f40110896a2eb00251098ac
somnek <mumscarpetiswet@gmail.com>
2024-02-05 20:51:51 +0800

    lsp

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ruff_lsp",  "gopls", "rust_analyzer", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.ruff_lsp.setup({})
+			lspconfig.gopls.setup({})
+			--
+			-- keymap
+			local opts = { noremap = true, silent = true }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		end,
+	},
+}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
-    config.setup ({
-      ensure_installed = {"go", "lua", "javascript", "python", "rust", "typescript"},
+    config.setup({
+      auto_install = true, -- automaticall install for unregconize language
+      ensure_installed = { "go", "lua", "javascript", "python", "rust", "typescript" },
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
-  end
+  end,
 }
